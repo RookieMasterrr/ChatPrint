54c90fd24df9af2b1bb681257accab63913e34a5
Rookieaster <68706543+RookieMasterrr@users.noreply.github.com>
2024-07-28 18:53:47 +0800

    bug-fix, 新增回答时无法响应并加上input框

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,38 @@
-console.log('content.js')
+consoleg('content.js')
+
+let element_length = 0
 
 const observer = new MutationObserver((mutationsList, observer) => {
-    const elements = document.querySelectorAll('div.w-full.text-token-text-primary');
 
-    if (elements.length > 0) {
-        console.log('length of elements', elements.length);
-        elements.forEach((element, index) => {
-            console.log("element =", element);
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox'
-            checkbox.checked = true
-            element.parentNode.insertBefore(checkbox, element);
-        });
-        observer.disconnect(); // 完成后停止观察
+    for(let mutation of mutationsList) {// 遍历变化
+        if(mutation.type == "childList") {// 如果是新增子元素
+            
+            // 遍历回答和问题
+            const elements = document.querySelectorAll('div.w-full.text-token-text-primary');
+
+            // 如果是新增回答或问题
+            if (elements.length!=element_length && elements.length > 0) {
+                console.log('change...')
+                // 重设长度
+                element_length = elements.length
+                // 新增input
+                elements.forEach((element, index) => {
+                    const checkbox = document.createElement('input');
+                    checkbox.type = 'checkbox'
+                    checkbox.checked = true
+                    element.parentNode.insertBefore(checkbox, element);
+                });
+            }
+        }
     }
+
+
+    
 });
 
-observer.observe(document.body, { childList: true, subtree: true });
+observer.observe(document.body, { 
+    childList: true, 
+    subtree: true,
+    attributes: true,
+    characterData: true 
+});
